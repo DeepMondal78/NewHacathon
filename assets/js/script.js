05f377289644141fc07be5be2bc6a66250f18b07
Deep Mondal <deepmondalkumar6@gmail.com>
2025-04-13 11:50:23 +0530

    Navber Animetion Add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,48 @@
 // document.addEventListener("DOMContentLoaded", () => {
 
 
-    var prevScrollpos = window.pageYOffset;
-    window.onscroll = function() {
-      var currentScrollPos = window.pageYOffset;
-      if (prevScrollpos > currentScrollPos) {
-        document.querySelector(".navbar").style.top = "0";
-      } else {
-        document.querySelector(".navbar").style.top = "-50px";
-      }
-      prevScrollpos = currentScrollPos;
-    }
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function () {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector(".navbar").style.top = "0";
+  } else {
+    document.querySelector(".navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
 
 
 
 
 // });
+
+// ==== NavMenu Mouse Hover Animetion ====
+function animateMenuItem(el) {
+  let letters = el.textContent.split('').map(letter => {
+    return `<span data-letter="${letter}">${letter}</span>`;
+  }).join('');
+  el.innerHTML = letters;
+}
+document.querySelectorAll('.menu-item').forEach(item => {
+  animateMenuItem(item);
+  let chars = item.querySelectorAll('span');
+
+  item.addEventListener('mouseenter', () => {
+    gsap.to(chars, {
+      y: '-100%',
+      stagger: 0.03,
+      ease: 'power4.out',
+      duration: 0.6
+    });
+  });
+
+  item.addEventListener('mouseleave', () => {
+    gsap.to(chars, {
+      y: '0%',
+      stagger: 0.03,
+      ease: 'power4.out',
+      duration: 0.6
+    });
+  });
+});
